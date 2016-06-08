be055d1ed65aba61ec6b5c8c0d97c6d8b768a855
Anthony Lapenna <lapenna.anthony@gmail.com>
2016-06-09 10:57:54 +1200

    use a consul server on each node

--- Vagrantfile
+++ Vagrantfile
@@ -1,31 +1,18 @@
-$prepare_consul_server_script = <<SCRIPT
-service docker stop;
-echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock"' | tee -a /etc/default/docker;
-service docker start;
-SCRIPT
-
 $prepare_swarm_manager_script = <<SCRIPT
 service docker stop;
 rm -rf /etc/docker/key.json
-echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --cluster-store=consul://10.0.7.10:8500 --cluster-advertise=eth1:2375"' | tee -a /etc/default/docker;
+echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --cluster-store=consul://localhost:8500 --cluster-advertise=eth1:2375"' | tee -a /etc/default/docker;
 service docker start;
 SCRIPT
 
 $prepare_swarm_node_script = <<SCRIPT
 service docker stop;
 rm -rf /etc/docker/key.json;
-echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --cluster-store=consul://10.0.7.10:8500 --cluster-advertise=eth1:2375"' | tee -a /etc/default/docker;
+echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --cluster-store=consul://localhost:8500 --cluster-advertise=eth1:2375"' | tee -a /etc/default/docker;
 service docker start;
 SCRIPT
 
 Vagrant.configure(2) do |config|
-  config.vm.define "consul" do |config|
-    config.vm.box = "deviantony/ubuntu-14.04-docker"
-    config.vm.hostname = "consul"
-    config.vm.network "private_network", ip: "10.0.7.10"
-    config.vm.provision "shell", inline: $prepare_consul_server_script
-    config.vm.synced_folder ".", "/vagrant", disabled: true
-  end
   config.vm.define "swarm_manager" do |config|
     config.vm.box = "deviantony/ubuntu-14.04-docker"
     config.vm.hostname = "swarm-manager"
--- start_cluster.sh
+++ start_cluster.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-# Run a Consul container
-echo "Starting Consul container..."
-docker -H 10.0.7.10:2375 run -d --restart always --net host -p 8500:8500 --name consul consul agent -bind 10.0.7.10 -client 10.0.7.10 -server -bootstrap-expect 1
+# Run Consul server on each node
+echo "Starting Consul containers..."
+docker -H 10.0.7.11:2375 run -d --restart always --name consul1 -e CONSUL_BIND_INTERFACE=eth0 --net host consul agent -server -retry-join 10.0.7.11 -retry-join 10.0.7.12 -retry-join 10.0.7.13 -bootstrap-expect 3
+docker -H 10.0.7.12:2375 run -d --restart always --name consul2 -e CONSUL_BIND_INTERFACE=eth0 --net host consul agent -server -retry-join 10.0.7.11 -retry-join 10.0.7.12 -retry-join 10.0.7.13 -bootstrap-expect 3
+docker -H 10.0.7.13:2375 run -d --restart always --name consul3 -e CONSUL_BIND_INTERFACE=eth0 --net host consul agent -server -retry-join 10.0.7.11 -retry-join 10.0.7.12 -retry-join 10.0.7.13 -bootstrap-expect 3
 
 # Run a Swarm manager node (HA enabled)
 echo "Starting Swarm manager..."
-docker -H 10.0.7.11:2375 run -d --restart always -p 4000:4000 --name swarm_manager swarm manage -H :4000 --replication --advertise 10.0.7.11:2375 consul://10.0.7.10:8500
+docker -H 10.0.7.11:2375 run -d --restart always -p 4000:4000 --name swarm_manager swarm manage -H :4000 --replication --advertise 10.0.7.11:2375 consul://localhost:8500
 
 # Run the first and second Swarm nodes
 echo "Starting Swarm node #1..."
-docker -H 10.0.7.12:2375 run -d --restart always --name swarm_node1 swarm join --heartbeat 20s --ttl 30s --advertise 10.0.7.12:2375 consul://10.0.7.10:8500
+docker -H 10.0.7.12:2375 run -d --restart always --name swarm_node1 swarm join --heartbeat 20s --ttl 30s --advertise 10.0.7.12:2375 consul://localhost:8500
 echo "Starting Swarm node #2..."
-docker -H 10.0.7.13:2375 run -d --restart always --name swarm_node2 swarm join --heartbeat 20s --ttl 30s --advertise 10.0.7.13:2375 consul://10.0.7.10:8500
+docker -H 10.0.7.13:2375 run -d --restart always --name swarm_node2 swarm join --heartbeat 20s --ttl 30s --advertise 10.0.7.13:2375 consul://localhost:8500
 
 echo "Waiting for the nodes to join the cluster..."
 # Wait for the nodes to join the cluster
