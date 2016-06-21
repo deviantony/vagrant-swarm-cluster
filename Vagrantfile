8288280366da1f5f83ec4895282dd296830b9b75
Anthony Lapenna <lapenna.anthony@gmail.com>
2016-06-21 16:37:08 +1200

    Swarm simple setup

--- Vagrantfile
+++ Vagrantfile
@@ -5,27 +5,12 @@ echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --cluste
 service docker start;
 SCRIPT
 
-$prepare_swarm_replica_script = <<SCRIPT
-service docker stop;
-rm -rf /etc/docker/key.json
-echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --cluster-store=consul://10.0.7.11:8500 --cluster-advertise=eth1:2375"' | tee -a /etc/default/docker;
-service docker start;
-SCRIPT
-
 $prepare_swarm_node1_script = <<SCRIPT
 service docker stop;
 rm -rf /etc/docker/key.json;
-echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --cluster-store=consul://10.0.7.12:8500 --cluster-advertise=eth1:2375"' | tee -a /etc/default/docker;
+echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --cluster-store=consul://10.0.7.11:8500 --cluster-advertise=eth1:2375"' | tee -a /etc/default/docker;
 service docker start;
 SCRIPT
-
-$prepare_swarm_node2_script = <<SCRIPT
-service docker stop;
-rm -rf /etc/docker/key.json;
-echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --cluster-store=consul://10.0.7.13:8500 --cluster-advertise=eth1:2375"' | tee -a /etc/default/docker;
-service docker start;
-SCRIPT
-
 
 Vagrant.configure(2) do |config|
   config.vm.define "swarm_manager" do |config|
@@ -36,27 +21,11 @@ Vagrant.configure(2) do |config|
     config.vm.synced_folder ".", "/vagrant", disabled: true
   end
 
-  config.vm.define "swarm_replica" do |config|
-    config.vm.box = "deviantony/ubuntu-14.04-docker"
-    config.vm.hostname = "swarm-replica"
-    config.vm.network "private_network", ip: "10.0.7.11"
-    config.vm.provision "shell", inline: $prepare_swarm_replica_script
-    config.vm.synced_folder ".", "/vagrant", disabled: true
-  end
-
   config.vm.define "swarm_node1" do |config|
     config.vm.box = "deviantony/ubuntu-14.04-docker"
     config.vm.hostname = "swarm-node1"
-    config.vm.network "private_network", ip: "10.0.7.12"
+    config.vm.network "private_network", ip: "10.0.7.11"
     config.vm.provision "shell", inline: $prepare_swarm_node1_script
-    config.vm.synced_folder ".", "/vagrant", disabled: true
-  end
-
-  config.vm.define "swarm_node2" do |config|
-    config.vm.box = "deviantony/ubuntu-14.04-docker"
-    config.vm.hostname = "swarm-node2"
-    config.vm.network "private_network", ip: "10.0.7.13"
-    config.vm.provision "shell", inline: $prepare_swarm_node2_script
     config.vm.synced_folder ".", "/vagrant", disabled: true
   end
 end
--- start_cluster.sh
+++ start_cluster.sh
@@ -2,46 +2,28 @@
 
 SWARM_VERSION=1.2.0
 
-# Run a Consul server on the first three nodes
+# Run a Consul server on the first node
 echo "Starting Consul containers..."
 docker -H 10.0.7.10:2375 run -d --restart always --name consul1 --net host \
 -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true}' \
 -e 'CONSUL_CLIENT_INTERFACE=eth1' -e 'CONSUL_BIND_INTERFACE=eth1' \
-consul agent -server -ui -bootstrap-expect 3
+consul agent -server -ui -bootstrap-expect 1
 
-docker -H 10.0.7.11:2375 run -d --restart always --name consul2 --net host \
+# Run a Consul agent on the second node
+docker -H 10.0.7.11:2375 run -d --restart always --name consul3 --net host \
 -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true}' \
--e 'CONSUL_CLIENT_INTERFACE=eth1' -e 'CONSUL_BIND_INTERFACE=eth1' \
-consul agent -server -retry-join 10.0.7.10
-
-docker -H 10.0.7.12:2375 run -d --restart always --name consul3 --net host \
--e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true}' \
--e 'CONSUL_CLIENT_INTERFACE=eth1' -e 'CONSUL_BIND_INTERFACE=eth1' \
-consul agent -server -retry-join 10.0.7.10
-
-# Run a Consul agent on last node
-docker -H 10.0.7.13:2375 run -d --restart always --name consul4 --net host \
--e 'CONSUL_LOCAL_CONFIG={"leave_on_terminate": true}' \
 -e 'CONSUL_CLIENT_INTERFACE=eth1' -e 'CONSUL_BIND_INTERFACE=eth1' \
 consul agent -retry-join 10.0.7.10
 
-# Run Swarm managers (HA enabled)
+# Run Swarm manager
 echo "Starting Swarm manager..."
 docker -H 10.0.7.10:2375 run -d --restart always -p 4000:4000 --name swarm_manager \
 swarm:${SWARM_VERSION} manage -H :4000 --replication --advertise 10.0.7.10:4000 consul://10.0.7.10:8500
 
-echo "Starting Swarm replica..."
-docker -H 10.0.7.11:2375 run -d --restart always -p 4000:4000 --name swarm_replica \
-swarm:${SWARM_VERSION} manage -H :4000 --replication --advertise 10.0.7.11:4000 consul://10.0.7.11:8500
-
-# Run the first and second Swarm nodes
-echo "Starting Swarm node #1..."
-docker -H 10.0.7.12:2375 run -d --restart always --name swarm_node1 \
-swarm:${SWARM_VERSION} join --heartbeat 20s --ttl 30s --advertise 10.0.7.12:2375 consul://10.0.7.12:8500
-
-echo "Starting Swarm node #2..."
-docker -H 10.0.7.13:2375 run -d --restart always --name swarm_node2 \
-swarm:${SWARM_VERSION} join --heartbeat 20s --ttl 30s --advertise 10.0.7.13:2375 consul://10.0.7.13:8500
+# Run Swarm node
+echo "Starting Swarm node..."
+docker -H 10.0.7.11:2375 run -d --restart always --name swarm_node1 \
+swarm:${SWARM_VERSION} join --heartbeat 20s --ttl 30s --advertise 10.0.7.11:2375 consul://10.0.7.11:8500
 
 # Swarm cluster status
 docker -H 10.0.7.10:4000 info
