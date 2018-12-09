fd639c295c495cbcbeeb26c39be8ce966f7c3025
Chaim Lev-Ari <chiptus@gmail.com>
2018-12-09 11:27:06 +0200

    add tcp protocol to ips

--- start_cluster.sh
+++ start_cluster.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Starting manager..."
-docker -H 10.0.7.10:2375 swarm init --advertise-addr 10.0.7.10
+docker -H tcp://10.0.7.10:2375 swarm init --advertise-addr 10.0.7.10
 
 echo "Retrieve manager token..."
-TOKEN=$(docker -H 10.0.7.10:2375 swarm join-token -q worker)
+TOKEN=$(docker -H tcp://10.0.7.10:2375 swarm join-token -q worker)
 
 echo "Starting node..."
-docker -H 10.0.7.11:2375 swarm join --token "${TOKEN}" 10.0.7.10:2377
+docker -H tcp://10.0.7.11:2375 swarm join --token "${TOKEN}" 10.0.7.10:2377
 
-docker -H 10.0.7.10:2375 info
+docker -H tcp://10.0.7.10:2375 info
